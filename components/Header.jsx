5f459b3793e7cf20dcf65de757f3ebbfc9ba4b97
egpennington <egpennington@hotmail.com>
2025-05-08 19:41:15 -0800

    refactor header and mobile style

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,13 @@
 export default function Header() {
   return (
     <div className="header">
-        <img src="./images/bwbulldogLogo.png" className="logo" alt="shadow outline of a bulldog" />
+        
         <div className="title">
           <p className="title-main">Sampson's Travel Journal.</p>
           <p>unleash adventure</p>
-        </div>        
+        </div>
         <img src="./images/tripLogo.png" className="trip-logo" alt="cartoon photos of a trip" />
+        <img src="./images/bwbulldogLogo.png" className="logo" alt="shadow outline of a bulldog" />        
     </div>
   )
 }
